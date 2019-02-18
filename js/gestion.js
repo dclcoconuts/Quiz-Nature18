d3d913f5a81ebe9e1e8029fe638e175341801e97
dclcoconuts <dcl.corinnem@18pixel.fr>
2019-02-18 16:54:11 +0100

    changement arbo

---
+++ js/gestion.js
@@ -0,0 +1,67 @@
+// pointeur sur la position de l'article courant dans le catalogue
+var index = 0;
+// initialisation du catalogue
+var catalogue = [];
+
+function executerRequete(callback) {
+    // on vérifie si le catalogue a déjà été chargé pour n'exécuter la requête AJAX
+    // qu'une seule fois
+    if (catalogue.length === 0) {
+        // on récupère un objet XMLHttpRequest
+        var xhr = getXMLHttpRequest();
+        // on réagit à l'événement onreadystatechange
+        xhr.onreadystatechange = function() {
+            // test du statut de retour de la requête AJAX
+            if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+                // on désérialise le catalogue et on le sauvegarde dans une variable
+                catalogue = JSON.parse(xhr.responseText);
+                // on lance la fonction de callback avec le catalogue récupéré
+                callback();
+            }
+        }
+        // la requête AJAX : lecture de data.json
+        xhr.open("GET", "bdd/quizz.json", true);
+        xhr.send();
+    } else {
+        // on lance la fonction de callback avec le catalogue déjà récupéré précédemment
+        callback();
+    }
+}
+
+function lireSuivant() {
+    // connaitre le nombre d'articles dans le catalogue
+    var longueur = catalogue.length;
+    // manipulation du DOM pour afficher les caractéristiques de l'article
+    document.getElementById("quest").innerHTML = catalogue[index].libelle_question;
+    // efface les div de réponse
+    for (i=0; i<8; i++)
+    {
+        var numdiv="cont"+i;
+        document.getElementById(numdiv).style.display = "none";
+    }  
+    // connaitre le nombre de réponse à afficher
+    nbReponse = catalogue[index].nbr_reponse;
+    for (i=0; i<nbReponse; i++)
+    {
+        var numlabel='lab'+i;
+        var numdiv="cont"+i;
+        document.getElementById(numdiv).style.display = "block";
+        document.getElementById(numlabel).textContent = catalogue[index].reponse[i].libelle_reponse;
+    }    
+    if (index < longueur - 1) {
+        index++;
+    }
+}
+
+// function lirePrecedent() {
+//     document.getElementById("nom").innerHTML = catalogue[index].nom;
+//     document.getElementById("desc").innerHTML = catalogue[index].desc;
+//     document.getElementById("qte").innerHTML = catalogue[index].qte;
+//     document.getElementById("prix").innerHTML = catalogue[index].prix;
+//     if (index > 0) {
+//         index--;
+//     }
+// }
+
+// on initialise la lecture au premier élément
+executerRequete(lireSuivant);
